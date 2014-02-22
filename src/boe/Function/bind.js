c8ebcd603bca273a2f66c7dfd65b0a8e2f0c3669
Norman Xu <homyu.shinn@gmail.com>
2014-02-22 02:25:53 +0000

    fix a err in bind.js

--- src/boe/Function/bind.js
+++ src/boe/Function/bind.js
@@ -3,7 +3,7 @@
  */
 define(['../util'], function (util) {
     // simply alias it
-    var FUNCTION_PROTO = global.Function.prototype;
+    var FUNCTION_PROTO = util.g.Function.prototype;
 
     return FUNCTION_PROTO.bind || util.bind;
 })
