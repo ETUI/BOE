5622902aa7c5aca6b7cb40320d22814dd8b4af34
Norman Xu <homyu.shinn@gmail.com>
2014-02-22 01:00:24 +0000

    move almond to npm

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,13 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+    var SPACE_NAME = 'boe';
+    var EXT_JS = '.js';
+    var EXT_JS_MIN = '.min' + EXT_JS;
+    var FILE_NAME_OUT_MAX = SPACE_NAME + EXT_JS;
+    var FILE_NAME_OUT_MIN = SPACE_NAME + EXT_JS_MIN;
+    var FILE_NAME_ENTRY = 'boe';
+
     grunt.loadNpmTasks("grunt-contrib-requirejs");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-bumpup");
@@ -11,16 +18,19 @@ module.exports = function(grunt) {
             dist : {
                 options : {
                     baseUrl: './src',
-                    name: '../components/almond/almond',
-                    include: 'boe',
-                    out: 'boe.js',
+                    name: '../node_modules/almond/almond',
+                    include: FILE_NAME_ENTRY,
+                    out: FILE_NAME_OUT_MAX,
                     wrap: {
                         start: 
                             "(function() { \n" + 
                             "var global = new Function('return this')();" + 
-                            "var parentDefine = global.define || (function(factory){ window.boe = factory(); }) ;",
+                            "var parentDefine = global.define || (function(factory){ " + 
+                                "var ret = factory();" +
+                                "typeof module != 'undefined' && (module.exports = ret) ||" +
+                                "(global." + SPACE_NAME + " = ret); }) ;",
                         end: 
-                            "parentDefine(function() { return require('boe'); }); \n" + 
+                            "parentDefine(function() { return require('" + SPACE_NAME + "'); }); \n" + 
                             "}());"
                     },
                     optimize : "none"
@@ -32,8 +42,8 @@ module.exports = function(grunt) {
                 banner : '/* http://github.com/normanzb/ */',
             },
             dist : {
-                src : [ "boe.js" ],
-                dest : "boe.min.js"
+                src : [ FILE_NAME_OUT_MAX ],
+                dest : FILE_NAME_OUT_MIN
             }
         },
         bumpup: {
